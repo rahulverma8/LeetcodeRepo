cfdda0819f8cc86988e767690a63ab3646be4dcd
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2023-12-13 22:04:22 +0530

    Update Maximum_Product_of_Two_Elements_in_an_Array.java

--- Maximum_Product_of_Two_Elements_in_an_Array.java
+++ Maximum_Product_of_Two_Elements_in_an_Array.java
@@ -1,6 +1,8 @@
 class Solution {
     public int maxProduct(int[] nums) {
         int maxnew=0,maxold=0,maxval=0;
+        //maxnew will hold the max value
+        //maxold will hold the 2nd max value
         for(int i=0;i<nums.length;i++){
             if(nums[i]>maxnew){
             maxold=maxnew;
