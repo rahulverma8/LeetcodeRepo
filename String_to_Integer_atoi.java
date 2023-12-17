a62cc5c11ecf82dd70a90e3f131e28cdc599cf82
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2023-12-17 18:50:04 +0530

    Create String_to_Integer_atoi.java

---
+++ String_to_Integer_atoi.java
@@ -0,0 +1,52 @@
+class Solution {
+
+    public int extractNum(String s,int signedOne){
+        String numStr = "";
+        boolean zeroCrossed=false;
+        if(s.length()==0) return 0;
+        //getting the number
+        for(int i=0;i<s.length();i++){
+            
+            if(s.length()>0 && s.charAt(i)>='0' && s.charAt(i)<='9'){
+
+                if(s.charAt(i)=='0'){
+                    if(!zeroCrossed) continue;
+                    else numStr += s.charAt(i);
+                }
+                else{
+                    zeroCrossed = true;
+                    numStr += s.charAt(i);
+                } 
+            } 
+            else break;
+        }
+        try {
+            int num=0;
+            if(numStr.length()!=0)
+                num = Integer.parseInt(numStr);
+            return num*signedOne;
+        } catch (NumberFormatException e) {
+            if(signedOne==1)    return Integer.MAX_VALUE;
+            else return Integer.MIN_VALUE;
+        }
+        //long intMaxVal = Integer.MAX_VAL;
+        //if(((long)intMaxVal)<num) System.out.println("");
+    }
+
+    public int myAtoi(String s) {
+       s = s.trim();
+       byte signedOne=1;
+       
+       //checking sign
+       if(s.length()==0) return 0; 
+       if(s.charAt(0)=='-'||s.charAt(0)=='+'){
+           if(s.charAt(0)=='-')signedOne=-1;
+
+           s = s.substring(1,s.length());
+       } 
+
+        int n = extractNum(s,signedOne);
+
+       return n;
+    }
+}
