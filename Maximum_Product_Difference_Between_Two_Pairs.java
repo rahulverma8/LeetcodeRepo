5a5dcdcff568293be85c60b0e297f00e2c44ee71
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2023-12-20 22:20:24 +0530

    Update Maximum_Product_Difference_Between_Two_Pairs.java

--- Maximum_Product_Difference_Between_Two_Pairs.java
+++ Maximum_Product_Difference_Between_Two_Pairs.java
@@ -4,3 +4,4 @@ class Solution {
         return (nums[nums.length-1]*nums[nums.length-2])-(nums[0]*nums[1]);
     }
 }
+//this problem can be solved without sorting also,we just have to get the two max and two min elements from the array.
