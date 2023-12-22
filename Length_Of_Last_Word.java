b28de288fe03a25737132b020a2b19e29de23496
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2023-12-22 23:41:41 +0530

    Update Length_Of_Last_Word.java

--- Length_Of_Last_Word.java
+++ Length_Of_Last_Word.java
@@ -4,10 +4,8 @@ public:
         int wordlen = 0;
         bool gotword = 0 ;
         for(int i=s.length()-1;i>=0;i--){
-
             if(s.at(i)!=' ')
                 gotword  = 1;
-            
             if(gotword){
 
                 if(s.at(i)==' ')
@@ -15,7 +13,6 @@ public:
                 else
                     wordlen++;
                 }
-
         }
         return wordlen;
     }
