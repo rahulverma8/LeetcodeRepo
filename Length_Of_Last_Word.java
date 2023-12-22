ef0a4584b9b8acb2a5721dfe43f5c3e526105b35
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2023-12-22 23:40:41 +0530

    Create Length_Of_Last_Word.java

---
+++ Length_Of_Last_Word.java
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int wordlen = 0;
+        bool gotword = 0 ;
+        for(int i=s.length()-1;i>=0;i--){
+
+            if(s.at(i)!=' ')
+                gotword  = 1;
+            
+            if(gotword){
+
+                if(s.at(i)==' ')
+                    break;
+                else
+                    wordlen++;
+                }
+
+        }
+        return wordlen;
+    }
+};
