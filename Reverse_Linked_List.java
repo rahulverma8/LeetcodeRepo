0ac13d89f83a857fd8551309c77ce4e325faac01
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2023-12-24 22:19:05 +0530

    Update Reverse_Linked_List.java

--- Reverse_Linked_List.java
+++ Reverse_Linked_List.java
@@ -17,3 +17,13 @@ class Solution {
         return head;
     }
 }
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
