60d0d720c7f3045b002e1d2183284d469aa3810c
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2023-12-27 19:12:40 +0530

    Update Search_in_a_Binary_Search_Tree.java

--- Search_in_a_Binary_Search_Tree.java
+++ Search_in_a_Binary_Search_Tree.java
@@ -9,3 +9,18 @@ class Solution {
         }
     }
 }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
