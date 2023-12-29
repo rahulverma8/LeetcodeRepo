664d3bc285e0e4f109a4797da00f0c51934b5f0d
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2023-12-29 23:46:39 +0530

    Update Middle_of_the_Linked_List.java

--- Middle_of_the_Linked_List.java
+++ Middle_of_the_Linked_List.java
@@ -15,3 +15,13 @@ class Solution {
         return temp;
     }
 }
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
