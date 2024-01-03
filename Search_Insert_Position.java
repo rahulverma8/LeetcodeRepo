fb464efa7c175ac2a1b63d4b9d84f2982ef3e80b
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2024-01-03 22:28:42 +0530

    Create Search_Insert_Position.java

---
+++ Search_Insert_Position.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l=0,r=nums.length-1,mid=0;
+
+        if(target<nums[0]) return 0;
+        if(target>nums[r]) return r+1;
+        
+        while(l<=r){
+            mid=(l+r)/2;
+            if(nums[mid]==target) return mid;
+
+            if(target<nums[mid]) r=mid-1;
+            else l=mid+1;
+        }
+        return l;
+    }
+}
