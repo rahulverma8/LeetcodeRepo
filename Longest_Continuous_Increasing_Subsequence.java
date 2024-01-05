93bd0639cbc7e6d69f08f9b4147db269ae8fc7b1
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2024-01-05 23:01:51 +0530

    Create Longest_Continuous_Increasing_Subsequence.java

---
+++ Longest_Continuous_Increasing_Subsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        int max=1;
+        int count=1;
+
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1] < nums[i]) count++;
+            else{
+                if(count>max) max = count;
+                count=1;
+            }
+        }
+        return Math.max(count,max);
+    }
+}
