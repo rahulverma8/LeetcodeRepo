d15cad9ffab630ff1e5a923587fda3697c8eed1c
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2024-01-07 21:28:58 +0530

    Update Range_Sum_of_BST.java

--- Range_Sum_of_BST.java
+++ Range_Sum_of_BST.java
@@ -14,3 +14,18 @@ class Solution {
         return sumofrange;
     }
 }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
