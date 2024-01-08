4cf69f1a1efec33006af8056c35b22c04cd5aacb
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2024-01-08 10:31:27 +0530

    Update Univalued_Binary_Tree.java

--- Univalued_Binary_Tree.java
+++ Univalued_Binary_Tree.java
@@ -16,3 +16,18 @@ class Solution {
         return isUniValued;
     }
 }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
