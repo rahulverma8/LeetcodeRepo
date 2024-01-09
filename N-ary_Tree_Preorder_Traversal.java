d0821c3bca506a1a8482382df00d5eda3fe23e22
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2024-01-09 17:11:48 +0530

    Update N-ary_Tree_Preorder_Traversal.java

--- N-ary_Tree_Preorder_Traversal.java
+++ N-ary_Tree_Preorder_Traversal.java
@@ -13,3 +13,21 @@ class Solution {
         return res;
     }
 }
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
