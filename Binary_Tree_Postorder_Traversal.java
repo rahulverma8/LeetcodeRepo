912697c859ad56f7094732e2b712a86ffa00b222
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2024-01-12 22:18:01 +0530

    Create Binary_Tree_Postorder_Traversal.java

---
+++ Binary_Tree_Postorder_Traversal.java
@@ -0,0 +1,13 @@
+class Solution {
+    List<Integer> postList = new ArrayList<>();
+    void postorder(TreeNode root){
+        if(root==null) return;
+        postorder(root.left);
+        postorder(root.right);
+        postList.add(root.val);
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        postorder(root);
+        return postList;
+    }
+}
