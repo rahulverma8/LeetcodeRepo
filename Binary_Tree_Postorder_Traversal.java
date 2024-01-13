d1218cb3b34fcb59d3234375ea695bfd823a31b3
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2024-01-13 23:41:16 +0530

    Update Binary_Tree_Postorder_Traversal.java

--- Binary_Tree_Postorder_Traversal.java
+++ Binary_Tree_Postorder_Traversal.java
@@ -11,18 +11,4 @@ class Solution {
         return postList;
     }
 }
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
