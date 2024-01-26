fa4ed7854c89180499949553dae95af23519155d
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2024-01-26 23:37:43 +0530

    Update Count the Digits That Divide a Number.java

--- Count the Digits That Divide a Number.java
+++ Count the Digits That Divide a Number.java
@@ -1,7 +1,6 @@
 class Solution {
     public int countDigits(int num) {
-        int tempnum=num;
-        int count=0;
+        int tempnum=num,count=0;
         while(tempnum>0){
             if(num%(tempnum%10)==0) count++;
             tempnum /= 10;
