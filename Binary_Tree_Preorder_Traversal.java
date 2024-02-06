a91ffe6517e80d0b1ca5d0b769245c4667870e21
rahulverma8 <71512323+rahulverma8@users.noreply.github.com>
2024-02-06 22:43:50 +0530

    Update Binary_Tree_Preorder_Traversal.java

--- Binary_Tree_Preorder_Traversal.java
+++ Binary_Tree_Preorder_Traversal.java
@@ -11,18 +11,3 @@ class Solution {
         return resList;
     }
 }
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
